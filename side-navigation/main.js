6c189e4496d7e5168d70c785d22281f18e0d1265
Mayur Gupta <mayurgupta7234@gmail.com>
2021-11-11 06:27:42 +0530

    done sidenav overlay

--- side-navigation/main.js
+++ side-navigation/main.js
@@ -1,1 +1,13 @@
 console.log("added main.js");
+
+// sidenav overlay example
+
+// Set the width of the side navigation to 30%
+function openNav() {
+    document.getElementById("mySidenav").style.width = "30%"
+}
+
+// Set the width of the side navigation to 0
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0"
+}
